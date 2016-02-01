434d8701075a16341b57faea848aecb59a90b159
Dakota Clark <dakota@allclarks.com>
2016-02-01 09:25:44 -0800

    Added some test code.

---
+++ Tests/PS.B2.Tests.ps1
@@ -0,0 +1,9 @@
+$Verbose = @{}
+if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
+{
+    $Verbose.add("Verbose",$True)
+}
+
+$PSVersion = $PSVersionTable.PSVersion.Major
+Import-Module $PSScriptRoot\..\PS.B2 -Force
+
