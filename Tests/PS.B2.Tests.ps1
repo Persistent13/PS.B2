c37d7d046061d778888b95cbbd1d25f883cf65e3
Dakota Clark <dakota@allclarks.com>
2016-07-26 15:48:41 -0700

    Adjusted the test setup.

--- Tests/PS.B2.Tests.ps1
+++ Tests/PS.B2.Tests.ps1
@@ -1,9 +1,20 @@
-$Verbose = @{}
-if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
-{
-    $Verbose.add("Verbose",$True)
+# Remove the module and then import the module
+$WorkspaceRoot = $(Get-Item $PSScriptRoot).Parent.FullName
+Remove-Module 'PS.B2' -ErrorAction Ignore
+Import-Module "$WorkspaceRoot\PS.B2\PS.B2.psd1" -Force
+
+if($global:key -eq $null) {
+    # Using json key.
+    $settings = Get-Content $PSScriptRoot\testSettings.json | ConvertFrom-Json
+    $global:key = $settings.ApiKey
 }
 
-$PSVersion = $PSVersionTable.PSVersion.Major
-Import-Module $PSScriptRoot\..\PS.B2 -Force
-
+InModuleScope PS.HealthChecks {
+    Describe "PS.B2 class tests" {
+        Context "Custom classes should instantiate" {
+            It "Should create object" {
+                $true | Should Not $true
+            }
+        }
+    }
+}
